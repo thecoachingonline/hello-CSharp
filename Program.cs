9321e574fc3c4a8453058c2100c087b01685e794
Y.Nanthachai <thecoachingonline@gmail.com>
2024-01-26 15:16:06 +0000

    SHA256

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string message = "สวัสดีชาวโลก";
+        byte[] hashValue;
+
+        using (HashAlgorithm hash = SHA256.Create())
+        {
+            hashValue = hash.ComputeHash(Encoding.UTF8.GetBytes(message));
+        }
+
+        Console.WriteLine("ข้อความเข้ารหัส: ");
+        foreach (byte b in hashValue)
+        {
+            Console.Write($"{b:x2}");
+        }
+    }
+}
